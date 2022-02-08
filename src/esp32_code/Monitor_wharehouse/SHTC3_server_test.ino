fbb6772c3df04f7cf135a96798e3d1d335303d98
pasanteinnovacion2bvc <pasanteinnovacion2@bvc.com.co>
2022-02-07 21:23:43 -0500

    test_sensor

--- src/esp32_code/Monitor_wharehouse/SHTC3_server_test.ino
+++ src/esp32_code/Monitor_wharehouse/SHTC3_server_test.ino
@@ -1,3 +1,4 @@
+#include "Wire.h"
 #include "WiFi.h"
 #include "WiFiClient.h"
 #include "Adafruit_SHTC3.h"
@@ -8,37 +9,41 @@ const char* ssid = "XXXXXX";
 const char* password = "XXXXXXX";
 const char* host = "http://54.233.181.170";
 
+#define I2C_SDA 2  
+#define I2C_SCL 15
+
+TwoWire SCHT3I2C = TwoWire(0);
 
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(115200);
-
+ SCHT3I2C.begin(I2C_SDA, I2C_SCL, 100000);
   while (!Serial)
     delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("SHTC3 test");
-  if (! shtc3.begin()) {
-    Serial.println("Couldn't find SHTC3");
-    while (1) delay(1);
-  }
-  Serial.println("Found SHTC3 sensor");
-
-
-  Serial.println();
-  Serial.println("Connecting to: ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.println(".");
+ status = shtc3.begin(0x76, &SCHT3I2C);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+//
+//  Serial.println();
+//  Serial.println("Connecting to: ");
+//  Serial.println(ssid);
+//
+//  WiFi.begin(ssid, password);
+//
+//  while (WiFi.status() != WL_CONNECTED){
+//    delay(500);
+//    Serial.println(".");
+//  }
+//
+//  Serial.println("");
+//  Serial.println("WiFi connected");
+//  Serial.println("IP address: ");
+//  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -51,51 +56,51 @@ void loop() {
   Serial.print("Temperatura: "); Serial.print(temp.temperature); Serial.println("°C \n");
   Serial.print("Humedad: "); Serial.print(humidity.relative_humidity); Serial.println("% rH \n");
 
-  delay(5000);
-
-
-  
-  Serial.println("Connecting to: ");
-  Serial.println(host);
-
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)){
-    Serial.println("Connection failed");
-    return;
-  }
-
-  // URL for the request
-
-  String url = "/datonuevo.php";
-  String key = "?dispositivo=bodega";
-  String data1 = "&temperatura=";
-  String data2 = "&humedad=";
-
-  Serial.println("Requesting URL: ");
-  Serial.println(url);
-
-  client.print(String("GET ")+ url + key + data1 + temp.temperature + data2 + humidity.relative_humidity + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
-    }
-
-    Serial.println();
-    Serial.println("Closing connection");
+//  delay(5000);
+//
+//
+//  
+//  Serial.println("Connecting to: ");
+//  Serial.println(host);
+//
+//  WiFiClient client;
+//  const int httpPort = 80;
+//  if (!client.connect(host, httpPort)){
+//    Serial.println("Connection failed");
+//    return;
+//  }
+//
+//  // URL for the request
+//
+//  String url = "/datonuevo.php";
+//  String key = "?dispositivo=bodega";
+//  String data1 = "&temperatura=";
+//  String data2 = "&humedad=";
+//
+//  Serial.println("Requesting URL: ");
+//  Serial.println(url);
+//
+//  client.print(String("GET ")+ url + key + data1 + temp.temperature + data2 + humidity.relative_humidity + " HTTP/1.1\r\n" +
+//                 "Host: " + host + "\r\n" +
+//                 "Connection: close\r\n\r\n");
+//
+//    unsigned long timeout = millis();
+//    while (client.available() == 0) {
+//        if (millis() - timeout > 5000) {
+//            Serial.println(">>> Client Timeout !");
+//            client.stop();
+//            return;
+//        }
+//    }
+//
+//    // Read all the lines of the reply from server and print them to Serial
+//    while(client.available()) {
+//        String line = client.readStringUntil('\r');
+//        Serial.print(line);
+//    }
+//
+//    Serial.println();
+//    Serial.println("Closing connection");
 
     delay(6000);
 
