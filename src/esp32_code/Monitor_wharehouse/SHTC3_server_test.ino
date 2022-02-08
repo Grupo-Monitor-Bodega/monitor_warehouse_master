8302ec61567cf934ad85a21968b115266d73caa3
pasanteinnovacion2bvc <pasanteinnovacion2@bvc.com.co>
2022-02-07 21:53:09 -0500

    test_server_conn

--- src/esp32_code/Monitor_wharehouse/SHTC3_server_test.ino
+++ src/esp32_code/Monitor_wharehouse/SHTC3_server_test.ino
@@ -1,89 +1,63 @@
 #include "Wire.h"
 #include "WiFi.h"
 #include "WiFiClient.h"
-#include "Adafruit_SHTC3.h"
+//#include "Adafruit_SHTC3.h"
+#include "HTTPClient.h"
 
-Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
+//Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
 
 const char* ssid = "XXXXXX";
 const char* password = "XXXXXXX";
-const char* host = "http://54.233.181.170";
 
 #define I2C_SDA 2  
 #define I2C_SCL 15
 
-TwoWire SCHT3I2C = TwoWire(0);
+//TwoWire SCHT3I2C = TwoWire(0);
 
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(115200);
- SCHT3I2C.begin(I2C_SDA, I2C_SCL, 100000);
-  while (!Serial)
-    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+// Serial.begin(115200);
+// SCHT3I2C.begin(I2C_SDA, I2C_SCL, 100000);
+//  while (!Serial)
+//    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+//
+//  Serial.println("SHTC3 test");
+// status = shtc3.begin(0x76, &SCHT3I2C);  
+//  if (!status) {
+//    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+//    while (1);
+//  }
 
-  Serial.println("SHTC3 test");
- status = shtc3.begin(0x76, &SCHT3I2C);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
   }
 
-//
-//  Serial.println();
-//  Serial.println("Connecting to: ");
-//  Serial.println(ssid);
-//
-//  WiFi.begin(ssid, password);
-//
-//  while (WiFi.status() != WL_CONNECTED){
-//    delay(500);
-//    Serial.println(".");
-//  }
-//
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-//  Serial.println("IP address: ");
-//  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  sensors_event_t humidity, temp;
-  
-  shtc3.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-  
-  Serial.print("Temperatura: "); Serial.print(temp.temperature); Serial.println("°C \n");
-  Serial.print("Humedad: "); Serial.print(humidity.relative_humidity); Serial.println("% rH \n");
-
-//  delay(5000);
-//
-//
+//  sensors_event_t humidity, temp;
 //  
-//  Serial.println("Connecting to: ");
-//  Serial.println(host);
-//
-//  WiFiClient client;
-//  const int httpPort = 80;
-//  if (!client.connect(host, httpPort)){
-//    Serial.println("Connection failed");
-//    return;
-//  }
-//
-//  // URL for the request
-//
-//  String url = "/datonuevo.php";
-//  String key = "?dispositivo=bodega";
-//  String data1 = "&temperatura=";
-//  String data2 = "&humedad=";
-//
-//  Serial.println("Requesting URL: ");
-//  Serial.println(url);
-//
-//  client.print(String("GET ")+ url + key + data1 + temp.temperature + data2 + humidity.relative_humidity + " HTTP/1.1\r\n" +
-//                 "Host: " + host + "\r\n" +
-//                 "Connection: close\r\n\r\n");
-//
+//  shtc3.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+//  
+//  Serial.print("Temperatura: "); Serial.print(temp.temperature); Serial.println("°C \n");
+//  Serial.print("Humedad: "); Serial.print(humidity.relative_humidity); Serial.println("% rH \n");
+
+  HTTPClient http;
+  //http.begin("http://54.233.181.170/datonuevo.php?dispositivo=bodega&temperatura="+temp.temperature+"&humedad="+humidity.relative_humidity); //Specify the URL
+  http.begin("http://54.233.181.170/datonuevo.php?dispositivo=bodega&temperatura=200&humedad=180"); //Specify the URL
+  int httpCode = http.GET(); 
+  if (httpCode > 0) { 
+    String payload = http.getString();
+    Serial.println(payload);
+  }else {
+    Serial.println("Error on HTTP request");
+  }
+  http.end();
 //    unsigned long timeout = millis();
 //    while (client.available() == 0) {
 //        if (millis() - timeout > 5000) {
